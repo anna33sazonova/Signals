9e1df326d823f2159b828527849ecad7b5e834d5
a.doudement <a.doudement@groupeonepoint.com>
2023-09-27 16:49:54 +0200

    feat: add register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,8 +1,31 @@
 import {Component} from "@angular/core";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {}
+export class RegisterComponent{
+
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  registerForm: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required, Validators.minLength(5), Validators.maxLength(20)],
+    email:  ['', Validators.required, Validators.email] ,
+    passwordField:this.formBuilder.group({
+      password:  ['', Validators.required, Validators.minLength(5), Validators.maxLength(20)],
+      confirmPassword:['', Validators.required] }, {validators: [Validators.required]})
+  });
+
+  onSubmit(){
+    console.log('ok submit');
+  }
+
+  onReset(){
+    console.log('reset')
+  }
+
+}
