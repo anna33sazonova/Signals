c3c6845263aabb0e6d70b1c4834ce5156a2b5da1
a.doudement <a.doudement@groupeonepoint.com>
2023-10-03 16:20:43 +0200

    feat: fix interceptor, add token storage service

--- src/app/common/auth.interceptor.ts
+++ src/app/common/auth.interceptor.ts
@@ -1,18 +1,34 @@
 import {Injectable} from "@angular/core";
-import {HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {AuthService} from "./auth.service";
+import {
+  HTTP_INTERCEPTORS,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {TokenStorageService} from "./token-storage.service";
+
+const TOKEN_HEADER_KEY = 'x-access-token';
 
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
-    intercept(req: HttpRequest<any>, next: HttpHandler) {
-      const authToken = this.authService.getToken();
-    req = req.clone({
-      setHeaders: {
-        Authorization: "Bearer " + authToken
-      }
-    });
-    return next.handle(req);
+  constructor(private token: TokenStorageService) {}
+
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authReq = req;
+    const token = this.token.getToken();
+    if(token !== null) {
+      authReq = req.clone({
+        headers: req.headers.set(TOKEN_HEADER_KEY, token)
+      });
+    }
+    return next.handle(authReq);
   }
 }
+
+export const authInterceptorProviders = [
+  {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+];
