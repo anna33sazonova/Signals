309e76780b1acdbab55f2d032c9da5fff14958f9
a.doudement <a.doudement@groupeonepoint.com>
2023-10-04 16:39:57 +0200

    feat: add home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../common/user.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  content!: string;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getPublicContent().subscribe(
+      data => this.content = data,
+      error => this.content = JSON.parse(error.error).message
+    );
   }
-
 }
