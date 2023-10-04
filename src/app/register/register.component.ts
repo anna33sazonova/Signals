918bc1b52af16055d3b8ec0aba16f88fe5c18a99
a.doudement <a.doudement@groupeonepoint.com>
2023-10-04 14:45:32 +0200

    feat: refactor register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,32 +1,63 @@
 import {Component} from "@angular/core";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {matchPasswordValidator} from "../common/match-password.validator";
+import {AuthService} from "../common/auth.service";
+import {UserSignUpForm} from "../data.model";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent{
+export class RegisterComponent {
 
   submitted = false;
+  signUpFailed = false;
+  errorMessage = '';
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  registerForm: FormGroup = this.formBuilder.group({
-    username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
-    email:  ['', [Validators.required, Validators.email]] ,
-    passwordField:this.formBuilder.group({
-      password:  ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
-      confirmPassword:['', Validators.required] }, {validators: matchPasswordValidator})
-  });
-
-  onSubmit(){
-    console.log('ok submit');
+  constructor(private formBuilder: FormBuilder, private auth: AuthService) {
   }
 
-  onReset(){
-    console.log('reset')
+  registerForm: FormGroup = this.formBuilder.group({
+    username: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)],
+      nonNullable: true
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true
+    }),
+    passwordField: this.formBuilder.group({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)],
+        nonNullable: true
+      }),
+      confirmPassword: new FormControl('', {validators: [Validators.required], nonNullable: true})
+    }, {validators: matchPasswordValidator})
+  });
+
+  onSubmit(): void {
+    const formData = this.registerForm.value;
+    const data: UserSignUpForm = {
+      username: formData.username,
+      email: formData.email,
+      password: formData.password
+    }
+    this.auth.register(data).subscribe(
+      data => {
+        console.log(data);
+        this.submitted = true;
+        this.signUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.signUpFailed = true;
+      }
+    )
+  }
+
+  onReset() {
+    this.registerForm.reset();
   }
 
 }
