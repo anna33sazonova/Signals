3c0c4cffdda8a167795fc4d2611a88f6afbd2c18
a.doudement <a.doudement@groupeonepoint.com>
2023-10-04 15:55:03 +0200

    feat: add info component, add token manage methods to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,19 +14,29 @@ export class HeaderComponent {
 
   @Input() isLoggedIn!: boolean;
 
-  constructor(private fBuilder: FormBuilder, private authService: AuthService, private router: Router, private token: TokenStorageService) {}
+  message!: string;
+
+  constructor(private fBuilder: FormBuilder, private authService: AuthService, private router: Router, private token: TokenStorageService) {
+  }
 
   connectionForm = this.fBuilder.group<UserSignInForm>({
-    username: new FormControl('', { validators: [Validators.required] , nonNullable: true}),
-    password: new FormControl('', { validators: [Validators.required] , nonNullable: true})
+    username: new FormControl('', {validators: [Validators.required], nonNullable: true}),
+    password: new FormControl('', {validators: [Validators.required], nonNullable: true})
   });
 
   submitConnectionForm(): void {
     if (this.connectionForm.valid) {
-      this.authService.login(this.connectionForm.getRawValue());
+      this.authService.login(this.connectionForm.getRawValue()).subscribe(
+        data => {
+          this.token.saveToken(data.accessToken);
+          this.token.saveUser(data);
+        },
+        error => {
+          this.message = error.error.message;
+        }
+      );
     }
-  //  this.router.navigate(['info']);
-    console.log('submit');
+    //  this.router.navigate(['info']);
   }
 
   logout(): void {
